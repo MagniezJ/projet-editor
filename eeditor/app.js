58ab7f6cfe57ca87a39edc2df615b33ac3bcb67e
justine <magniezjustinepro@gmail.com>
2020-08-12 09:59:01 +0200

    finish

--- eeditor/app.js
+++ eeditor/app.js
@@ -11,5 +11,41 @@ text.addEventListener('keyup', function () {
         document.getElementById("compteur").innerHTML = " ";
     document.getElementById("p").innerHTML="";
     }
+    if (css.innerText != 0){
+        css.value="<style type=\"text/css\"></style>" +css.value;
+    }
     
 });
+
+
+let bold=document.getElementById("bold");
+bold.addEventListener('click', (e)=> {
+   /*  document.execCommand("bold", false, null) */
+   text.value= text.value+"<b>"+"</b>";
+   document.getElementById("story").innerHTML=text.value;
+});
+
+let italic=document.getElementById("italic");
+italic.addEventListener('click', (f)=> {
+   /*  document.execCommand("bold", false, null) */
+   text.value= text.value+"<i>"+"</i>";
+   document.getElementById("story").innerHTML=text.value;
+});
+
+let css= document.getElementById("css");
+let scss=document.getElementById("scss");
+let style=document.getElementById("style");
+let sstyle=document.getElementById("sendstyle");
+style.addEventListener('click',(f)=>{
+    document.getElementById("css").style.visibility = "visible";
+    css.value="<style type=\"text/css\"></style>" +css.value;
+    document.getElementById("css").innerHTML=css.value;
+    document.getElementById("sendstyle").style.visibility="visible";
+    document.getElementById("scss").style.visibility = "visible";
+});
+
+sstyle.addEventListener('click',(e)=>{
+    p.value=css.value+text.value;
+    document.getElementById('p').innerHTML=p.value;
+})
+
